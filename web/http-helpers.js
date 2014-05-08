5b65b4511c84420c30545034d6651a9d307cd52b
Hack Reactor Students <taylor.harwin@gmail.com>
2014-05-07 21:29:56 -0700

    Serves front page

--- web/http-helpers.js
+++ web/http-helpers.js
@@ -1,18 +1,86 @@
 var path = require('path');
 var fs = require('fs');
 var archive = require('../helpers/archive-helpers');
+var url = require('url');
 
 exports.headers = headers = {
   "access-control-allow-origin": "*",
   "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
   "access-control-allow-headers": "content-type, accept",
   "access-control-max-age": 10, // Seconds.
-  'Content-Type': "text/html"
+  'Content-Type': "application/json"
 };
 
-exports.serveAssets = function(res, asset) {
+exports.serveAssets = function(req, res) {
+
+// asset = archive.paths.siteAssets + '/index.html';
+//   fs.readFile(asset, 'utf8', function(err, data){
+//     if (err){
+//       console.error(err);
+//       res.end(404);
+//     } else {
+//       res.end(data);
+//     }
+//   });
+
+var pathName = url.parse(req.url);
+  console.log(pathName.pathname);
+  if (pathName.pathname === '/'){
+    fs.readFile(archive.paths.siteAssets + '/index.html', 'utf8', function(err, data){
+      if (err){
+        console.error(err);
+        res.end(404);
+      } else {
+        res.end(data);
+      }
+    });
+  } else if (pathName.pathname === '/styles.css'){
+    fs.readFile(archive.paths.siteAssets + '/styles.css', 'utf8', function(err, data){
+      if (err){
+        console.error(err);
+        res.end(404);
+      } else {
+        console.log('STYLES!')
+        res.end(data);
+      }
+    });
+  } else if (pathName.pathname === '/bower_components/jquery/dist/jquery.min.js'){
+    fs.readFile(archive.paths.siteAssets + '/bower_components/jquery/dist/jquery.min.js', 'utf8', function(err, data){
+      if (err){
+        console.error(err);
+        res.end(404);
+      } else {
+        console.log("JQUERY!");
+        res.end(data);
+      }
+    });
+  }
+};
+
+
+
   // Write some code here that helps serve up your static files!
   // (Static files are things like html (yours or archived from others...), css, or anything that doesn't change often.)
-};
+
 
 // As you progress, keep thinking about what helper functions you can put here!
+//
+
+
+
+
+
+
+
+
+// asset = asset || archive.paths.siteAssets + '/index.html';
+//   // var ourScript = archive.paths.siteAssets +  '/styles.css';
+//   // var jayQuery =
+//   fs.readFile(asset, 'utf8', function(err, data){
+//     if (err){
+//       console.error(err);
+//       res.end(404);
+//     } else {
+//       res.end(data);
+//     }
+//   });
--- web/request-handler.js
+++ web/request-handler.js
@@ -1,12 +1,17 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
+var http = require('./http-helpers');
 // require more modules/folders here!
 
 
 
 exports.handleRequest = function (req, res) {
   if(req.method === 'POST'){
+    http.serveAssets(res);
     archive.readListOfUrls(req, res);
   }
-  res.end(archive.paths.list);
+  if(req.method === 'GET'){
+    http.serveAssets(req, res);
+  }
+  // res.end(archive.paths.list);
 };
